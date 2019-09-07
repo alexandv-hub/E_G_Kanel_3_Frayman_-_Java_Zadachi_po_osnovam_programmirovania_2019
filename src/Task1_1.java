08dfdb2f54a13eb26717810fb297088e30e4235e
nikolajred <nikolajred@gmail.com>
2019-09-07 16:47:17 +0300

    first commit

---
+++ src/Task1_1.java
@@ -0,0 +1,23 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+        int 1x;
+        double t;
+        int t;
+        int f, f;
+        int x, X;
+        double a;
+        a1;
+        String kniga1;
+        kniga2;
+        char znak1, znak2, znak_1;
+
+    }
+
+    int x, a, d;
+    double y, z, x;
+
+    public void method() {
+        int x;
+        double X;
+    }
+}
