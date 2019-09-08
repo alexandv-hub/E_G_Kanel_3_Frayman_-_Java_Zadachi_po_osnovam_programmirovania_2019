510ad446b8d2d7c6789acd2a1e0dc220e55b43b3
nikolajred <nikolajred@gmail.com>
2019-09-08 19:26:26 +0300

    second commit

---
+++ src/Task2_15.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task2_15 {
+    static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("input variable a: ");
+        int a = scn.nextInt();
+        System.out.println("your variable a less than " + (a * 10));
+
+    }
+}
---
+++ src/Task2_16.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task2_16 {
+    static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("input variable a: ");
+        int a = scn.nextInt();
+        System.out.println(a+6);
+        System.out.println(a-12);
+    }
+}
